678330f1b882970da3e309c88e34545b6b60a5b3
7minutech <elijohnson1107@gmail.com>
2024-07-10 16:33:18 -0500

    Moved zip code validation into method

--- lib/event_manger.rb
+++ lib/event_manger.rb
@@ -1,4 +1,17 @@
 require 'csv'
+
+def clean_zip_code(zip_code)
+  if zip_code.nil?
+    "00000"
+  elsif zip_code.length < 5
+    zip_code.rjust(5,"0")
+  elsif zip_code.length > 5
+    zip_code[0..4]
+  else
+    zip_code
+  end
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -8,14 +21,6 @@ contents = CSV.open(
   )
 contents.each do |row|
   name = row[:first_name]
-  zip_code = row[:zipcode]
-  if zip_code.nil?
-    zip_code = "00000"
-  elsif zip_code.length < 5
-    zip_code = zip_code.rjust(5,"0")
-  elsif zip_code.length > 5
-    zip_code = zip_code[0..4]
-  end
-
+  zip_code = clean_zip_code(row[:zipcode])
   puts "#{name} #{zip_code}"
 end
