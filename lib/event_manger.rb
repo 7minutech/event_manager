0e2ee26d7f237f8f34b16775a809d2f83d2ddf1a
7minutech <elijohnson1107@gmail.com>
2024-07-10 16:35:50 -0500

    Refactored clean zip code method

--- lib/event_manger.rb
+++ lib/event_manger.rb
@@ -1,15 +1,7 @@
 require 'csv'
 
 def clean_zip_code(zip_code)
-  if zip_code.nil?
-    "00000"
-  elsif zip_code.length < 5
-    zip_code.rjust(5,"0")
-  elsif zip_code.length > 5
-    zip_code[0..4]
-  else
-    zip_code
-  end
+  zip_code.to_s.rjust(5,'0')[0..4]
 end
 
 puts 'Event Manager Initialized!'
