82674e2efb1eca32912945d3c112ee87841387a7
7minutech <elijohnson1107@gmail.com>
2024-07-10 16:05:30 -0500

    Read through csv and get first names

--- lib/event_manger.rb
+++ lib/event_manger.rb
@@ -1,1 +1,14 @@
 puts 'Event Manager Initialized!'
+
+sample_data = 'event_attendees.csv'
+if File.exist? sample_data
+  contents = File.read sample_data
+  lines = File.readlines(sample_data)
+    lines.each do |line|
+      cols = line.split(",")
+      name = cols[2]
+      puts name
+    end
+else
+  puts "File not there"
+end
