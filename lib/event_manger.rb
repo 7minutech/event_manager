3530af1666fe08aaff35d215bcdc1d1de3ba961e
7minutech <elijohnson1107@gmail.com>
2024-07-10 16:11:04 -0500

    Used each with index to skip header row

--- lib/event_manger.rb
+++ lib/event_manger.rb
@@ -4,10 +4,10 @@ sample_data = 'event_attendees.csv'
 if File.exist? sample_data
   contents = File.read sample_data
   lines = File.readlines(sample_data)
-    lines.each do |line|
-      cols = line.split(",")
-      name = cols[2]
-      puts name
+    lines.each_with_index do |line,index|
+      col = line.split(",")
+      next if index == 0
+      puts col[2]
     end
 else
   puts "File not there"
