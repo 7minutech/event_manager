7181190b55ba11b6e4e8d6e4206cd6c93d900625
7minutech <elijohnson1107@gmail.com>
2024-07-10 16:29:09 -0500

    Added zip code validation

--- lib/event_manger.rb
+++ lib/event_manger.rb
@@ -9,5 +9,13 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zip_code = row[:zipcode]
+  if zip_code.nil?
+    zip_code = "00000"
+  elsif zip_code.length < 5
+    zip_code = zip_code.rjust(5,"0")
+  elsif zip_code.length > 5
+    zip_code = zip_code[0..4]
+  end
+
   puts "#{name} #{zip_code}"
 end
